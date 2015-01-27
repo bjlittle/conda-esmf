2e8616a908545eba6cab5a89a98f4df18c8aceac
Ben Koziol <ben.koziol@noaa.gov>
2015-01-27 18:09:49 +0000

    mark - modifications to build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --depth=1 git://git.code.sf.net/p/esmf/esmf esmf
+git clone --depth 1 git://git.code.sf.net/p/esmf/esmf esmf
 cd esmf/
 export ESMF_DIR=`pwd`
 export ESMF_INSTALL_PREFIX=`pwd`/conda_install
@@ -8,6 +8,6 @@ make -j 4
 make install
 export ESMFMKFILE=`find $ESMF_INSTALL_PREFIX -name '*.mk'`
 cd src/addon/ESMPy/
-$PYTHON setup.py build --ESMFMKFILE=$ESMFMKFILE
-$PYTHON setup.py install
+python setup.py build --ESMFMKFILE=$ESMFMKFILE
+python setup.py install
 
