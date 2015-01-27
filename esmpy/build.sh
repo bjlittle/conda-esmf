1ad074a1c8fc5a934bc33e97f3e9ceb0148b1e86
Ben Koziol <benkoziol@gmail.com>
2015-01-27 12:10:19 -0700

    mark - testing

--- esmpy/build.sh
+++ esmpy/build.sh
@@ -2,7 +2,7 @@
 
 export ESMFMKFILE=`find ${PREFIX} -name '*.mk'`
 ESMPY_SRC=`find . -name '*src*addon*ESMPy*'`
-echo ESMPY_SRC
+echo $ESMPY_SRC
 exit 1
 ${PYTHON} setup.py build --ESMFMKFILE=${ESMFMKFILE}
 ${PYTHON} setup.py install
