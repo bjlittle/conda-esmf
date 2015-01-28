48501c8cbe2588d253a94e297e3e79d80880cd52
Ben Koziol <benkoziol@gmail.com>
2015-01-28 15:36:24 -0700

    mark - testing

--- esmf/build.sh
+++ esmf/build.sh
@@ -3,9 +3,8 @@
 export ESMF_DIR=`pwd`
 export ESMF_INSTALL_PREFIX=${PREFIX}
 export ESMF_NETCDF="split"
-
-echo ${PREFIX}
-exit 1
+export ESMF_NETCDF_INCLUDE=${PREFIX}/include
+export ESMF_NETCDF_LIBPATH=${PREFIX}/lib
 
 make -j 4
 make install
