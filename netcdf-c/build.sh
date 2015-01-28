5686ca675fb4d79f7f2dbd1a43f3798a1ba3485c
Ben Koziol <benkoziol@gmail.com>
2015-01-28 14:58:57 -0700

    mark - testing

---
+++ netcdf-c/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export CFLAGS="-I${PREFIX}/include $CFLAGS"
+export LDFLAGS="-L${PREFIX}/lib $LDFLAGS"
+
+./configure \
+    --enable-shared \
+    --enable-netcdf-4 \
+    --enable-dap \
+    --prefix=${PREFIX}
+make
+make install
