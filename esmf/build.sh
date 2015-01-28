b25e3848e59910ec7ad4653ddfeec99f3518da64
Ben Koziol <benkoziol@gmail.com>
2015-01-28 15:32:52 -0700

    mark - testing

--- esmf/build.sh
+++ esmf/build.sh
@@ -4,7 +4,7 @@ export ESMF_DIR=`pwd`
 export ESMF_INSTALL_PREFIX=${PREFIX}
 export ESMF_NETCDF="split"
 
-echo `find . -name '*include'`
+echo `find . -name '${PREFIX}*include*'`
 exit 1
 
 make -j 4
