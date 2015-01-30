37a9b5b1994ea5894ca9fe0b69c5210e0684ee54
Ben Koziol <benkoziol@gmail.com>
2015-01-30 10:32:28 -0700

    mark - testing

--- gcc/build.sh
+++ gcc/build.sh
@@ -9,6 +9,9 @@ export CFLAGS="-I${PREFIX}/include $CFLAGS"
 
 ${SRCDIR}/configure --prefix=${OBJDIR} --enable-languages=c,fortran --disable-multilib
 
+echo `ls`
+exit 1
+
 make -j 4
 make install
 
