56e25daa5c07f388c7cb17d0993027e915c1bcaa
Ben Koziol <benkoziol@gmail.com>
2015-01-30 10:00:49 -0700

    mark - testing

--- gcc/build.sh
+++ gcc/build.sh
@@ -2,7 +2,7 @@
 
 export CFLAGS="-I${PREFIX}/include $CFLAGS"
 
-./configure --prefix=${PREFIX} --enable-languages=c,fortran
+./configure --prefix=${PREFIX} --enable-languages=c,fortran --disable-multilib
 
 make -j 4
 make install
