c0fe95e64848a7a622e8da583b87b20afb1f9588
Ben Koziol <benkoziol@gmail.com>
2015-02-10 10:06:52 -0700

    successful build of esmpy

--- upload.py
+++ upload.py
@@ -7,5 +7,5 @@ ROOT = '/home/ubuntu/git/conda-esmf'
 
 for pkg in PKGS:
     os.chdir(os.path.join(ROOT, pkg))
-    to_upload = check_output(['conda', 'build', '--output', '.'])
+    to_upload = check_output(['conda', 'build', '--output', '.']).strip()
     check_call(['binstar', 'upload', to_upload])
