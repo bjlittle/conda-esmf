713336eb534777ed279ea36578a685026f509096
Ben Koziol <benkoziol@gmail.com>
2015-02-09 18:49:27 -0700

    mark - testing

--- mpc/build.sh
+++ mpc/build.sh
@@ -4,7 +4,12 @@
 
 export CFLAGS="-I${PREFIX}/include $CFLAGS"
 
-./configure --prefix=${PREFIX}
+#./configure --prefix=${PREFIX}
+./configure --prefix=${PREFIX}/mpc \
+            --with-gmp-include=${PREFIX}/gmp/include \
+            --with-gmp-lib=${PREFIX}/gmp/lib \
+            --with-mpfr-include=${PREFIX}/mpfr/include \
+            --with-mpfr-lib=${PREFIX}/mpfr/lib
 
 make
 make install
