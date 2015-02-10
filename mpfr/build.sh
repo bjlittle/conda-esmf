438c819ebe7499270bce935aae78aa6b63e8a8ce
Ben Koziol <benkoziol@gmail.com>
2015-02-09 18:45:58 -0700

    mark - testing

--- mpfr/build.sh
+++ mpfr/build.sh
@@ -2,7 +2,8 @@
 
 export CFLAGS="-I${PREFIX}/include $CFLAGS"
 
-./configure --prefix=${PREFIX}
+#./configure --prefix=${PREFIX}
+./configure prefix=${PREFIX}/mpfr --with-gmp-include=${PREFIX}/gmp/include --with-gmp-lib=${PREFIX}/gmp/lib
 
 make
 make install
