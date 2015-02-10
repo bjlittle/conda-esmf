c3dccc3cb1222a2d4be17a249b6062c59fa0e56d
Ben Koziol <benkoziol@gmail.com>
2015-02-10 10:05:24 -0700

    successful build of esmpy

---
+++ upload.py
@@ -0,0 +1,11 @@
+import os
+from subprocess import check_output, check_call
+
+
+PKGS = ['gmp', 'mpfr', 'mpc', 'gcc', 'netcdf-c', 'netcdf-fortran', 'esmf', 'esmpy']
+ROOT = '/home/ubuntu/git/conda-esmf'
+
+for pkg in PKGS:
+    os.chdir(os.path.join(ROOT, pkg))
+    to_upload = check_output(['conda', 'build', '--output', '.'])
+    check_call(['binstar', 'upload', to_upload])
